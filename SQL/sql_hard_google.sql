369f3d8490bb973bad1b60b17912c6ba7276462f
Milos Popovic <37574511+milos-agathon@users.noreply.github.com>
2021-06-28 13:39:41 +0200

    Update sql_hard_google.sql

--- SQL/sql_hard_google.sql
+++ SQL/sql_hard_google.sql
@@ -1,6 +1,12 @@
--- HARD
--- source: https://platform.stratascratch.com/coding-question?id=10165&python=
--- create table
+/*HARD
+source: https://platform.stratascratch.com/coding-question?id=10165&python=
+
+Find the email activity rank for each user. Email activity rank is defined by the total number of emails sent. 
+The user with the highest number of emails sent will have a rank of 1, and so on. Output the user, total emails, and their activity rank. 
+Order records by the total emails in descending order. Sort users with the same rank score in alphabetical order.
+In your rankings, return a unique value (i.e., a unique percentile) even if multiple users have the same number of emails.
+
+create table*/
 
 CREATE TABLE google_gmail_emails(
      id 	    VARCHAR(3),
