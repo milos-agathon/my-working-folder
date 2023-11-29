ff613d78935fce34719aec1c2fbcb9860889c33e
Milos Popovic <37574511+milos-agathon@users.noreply.github.com>
2023-11-29 16:31:06 +0100

    Add files via upload

--- R/interactive_population.r
+++ R/interactive_population.r
@@ -1,50 +1,42 @@
-setwd("D:/kontur")
+setwd("/Users/mpopovic3/Downloads/kontur")
 
 # define libraries we need
 libs <- c(
-    "tidyverse", "saqgetr",
-    "terra", "sf",
-    "rayshader", "geodata",
-    "deckgl", "kriging"
+    "R.utils", "sf",
+    "scales", "deckgl"
 )
 
 # install missing libraries
 installed_libraries <- libs %in% rownames(installed.packages())
 if (any(installed_libraries == F)) {
-    installed.packages(libs[!installed_libraries])
+    install.packages(libs[!installed_libraries])
 }
 
 # load libraries
 invisible(lapply(libs, library, character.only = T))
 
 options(timeout = 300)
-url <- "https://geodata-eu-central-1-kontur-public.s3.amazonaws.com/kontur_datasets/kontur_population_NL_20231101.gpkg.gz"
+url <- "https://geodata-eu-central-1-kontur-public.s3.amazonaws.com/kontur_datasets/kontur_population_BE_20231101.gpkg.gz"
+filename <- basename(url)
 
 download.file(
     url = url,
     destfile = basename(url),
     mode = "wb"
 )
+
 R.utils::gunzip(basename(url), remove = F)
 
 pop_df <- sf::st_read(
-    dsn = "D:/kontur/kontur_population_NL_20231101.gpkg"
+    dsn = gsub(
+        pattern = ".gz",
+        replacement = "",
+        x = filename
+    )
 ) |>
     sf::st_transform(crs = "EPSG:4326")
 
-props <- list(
-    pickable = TRUE,
-    stroked = TRUE,
-    filled = TRUE,
-    wireframe = TRUE,
-    lineWidthMinPixels = 1,
-    getPolygon = ~geom,
-    getElevation = deckgl::JS("d => d.population"),
-    getFillColor = deckgl::JS("d => [d.population / 60, 140, 0]"),
-    getLineColor = ~color,
-    getLineWidth = 1,
-    tooltip = "Population: {{population}}"
-)
+plot(sf::st_geometry(pop_df))
 
 pal <- scales::col_quantile(
     "viridis", pop_df$population,
@@ -55,6 +47,26 @@ pop_df$color <- pal(
     pop_df$population
 )
 
+deckgl::does_it_work()
+
+properties <- list(
+    pickable = T,
+    stroked = F,
+    filled = T,
+    extruded = T,
+    wireframe = T,
+    fp64 = T,
+    elevationScale = 1,
+    lineWidthMinPixels = 1,
+    getFillColor = ~color,
+    getElevation = ~population,
+    getLineColor = ~color,
+    getLineWidth = 1,
+    getPolygon = deckgl::JS("d => d.geom.coordinates"),
+    tooltip = "Population: {{population}}",
+    opacity = .8
+)
+
 deckgl::deckgl(
     latitude = 52.252365,
     longitude = 5.3350558,
@@ -62,15 +74,7 @@ deckgl::deckgl(
 ) |>
     deckgl::add_polygon_layer(
         data = pop_df,
-        getPolygon = JS("d => d.geom.coordinates"),
-        opacity = .1,
-        stroked = FALSE,
-        filled = TRUE,
-        extruded = TRUE,
-        wireframe = TRUE,
-        fp64 = TRUE,
-        getElevation = ~population,
-        getFillColor = ~color,
-        getLineColor = ~color
+        properties = properties
     ) |>
+    deckgl::add_legend_pal(pal, title = "Population") |>
     deckgl::add_basemap(deckgl::use_carto_style())
