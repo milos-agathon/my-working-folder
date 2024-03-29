9b2c66c9378f9f7d67b002f7c73e4998021c9d2f
Milos Popovic <37574511+milos-agathon@users.noreply.github.com>
2024-03-29 09:12:42 +0100

    Add files via upload

--- R/54-video.r
+++ R/54-video.r
@@ -1,5 +1,7 @@
 # 1. LIBRARIES
 
+install.packages("httr")
+
 pacman::p_load(
     rstac,
     sf,
@@ -10,6 +12,8 @@ ms_data_source <- rstac::stac(
     "https://planetarycomputer.microsoft.com/api/stac/v1"
 )
 
+ms_data_source
+
 ms_collections_query <- ms_data_source |>
     rstac::collections()
 
@@ -19,28 +23,28 @@ class(ms_collections_query)
 ms_collections <- rstac::get_request(ms_collections_query)
 print(ms_collections, n = 123)
 
-lux_bbox <- giscoR::gisco_get_countries(
-    country = "LUX",
+bbox <- giscoR::gisco_get_countries(
+    country = "SG",
     resolution = "1"
 ) |> sf::st_bbox()
 
-rstac::stac_search(
+ms_buildings_query <- rstac::stac_search(
     q = ms_data_source,
     collections = "hrea",
-    # datetime = "2021-01-01/2021-12-31",
-    bbox = lux_bbox,
-    limit = 999
-)
-
-ms_buildings_query <- rstac::get_request(ms_data_source)
+    datetime = "2019-12-31",
+    bbox = bbox,
+    limit = 100
+) |>
+    rstac::get_request()
 
 ms_query_signin <- rstac::items_sign(
-  ms_buildings_query,
-  rstac::sign_planetary_computer()
+    ms_buildings_query,
+    rstac::sign_planetary_computer()
 )
 
 rstac::assets_download(
-    ms_query_signin, 
-    "stac-items", 
-    output_dir = getwd
+    items = ms_query_signin,
+    asset_names = "estimated-brightness",
+    output_dir = getwd(),
+    overwrite = TRUE
 )
